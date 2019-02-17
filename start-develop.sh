4493e9391c15d1220a78aac013d4a1094e09e89a
itigoppo <itigoppo+github@gmail.com>
2019-02-18 03:41:17 +0900

    update start-develop.sh

--- start-develop.sh
+++ start-develop.sh
@@ -4,9 +4,13 @@ echo 'Setup application. [Y/n] > '
 read ANS
 case ${ANS} in
     [Yy] )
-        if [ -d '/Users/itigoppo/git/docker-nono150/apps/nono150_com' ]; then
+        if [ -d '/Users/itigoppo/git/nono150_com' ]; then
             echo '--- Symlink Create. nono150'
             ln -nfs ~/git/nono150_com/ ~/git/docker-nono150/apps/
+        fi
+        if [ -d '/Users/itigoppo/git/hpbg' ]; then
+            echo '--- Symlink Create. hpbg'
+            ln -nfs ~/git/hpbg/ ~/git/docker-nono150/apps/
         fi
         ;;
     * ) echo '--- Skip.' ;;
